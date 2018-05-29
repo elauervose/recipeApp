efc0011c365545a528550938a9599c1f263d2aa5
Erica Lauer Vose <elauervose@gmail.com>
2018-05-28 21:02:22 -0700

    working json on recipies#index

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,22 +2,25 @@ $(document).ready(function() {
   "use strict";
 
   // Call recipe endpoint
-  $.getJSON("/recipes.json").done(makeRecipeExcerpts).fail(function() {
-    console.log( "error" );
-  })
+  $.getJSON("/recipes")
+    .done(makeRecipeExcerpts)
+    .fail(function() {
+      console.log( "error" );
+    })
 });
 
 function makeRecipeExcerpts(excerpts){
-  $.each(excerpts, function(excerpt) {
+  // console.log(excerpts);
+  $.each(excerpts, function(index, excerpt) {
+    // console.log(excerpt);
     var tmplRecipeExcerpt = $($("#template-recipe-excerpt").html());
-    console.log(excerpt);
-    $("a.recipe-url", tmplRecipePage).attr(data.url);
-    $("a.recipe-url2", tmplRecipePage).attr(data.url);
-    $("a.recipe-url3", tmplRecipePage).attr(data.url);
-    $("img.recipe-image", tmplRecipePage).attr(data.image);
+    $("a.recipe-url", tmplRecipeExcerpt).attr(excerpt.url);
+    $("a.recipe-url2", tmplRecipeExcerpt).attr(excerpt.url);
+    $("a.recipe-url3", tmplRecipeExcerpt).attr(excerpt.url);
+    $("img.recipe-image", tmplRecipeExcerpt).attr(excerpt.image);
 
-    $("div.inner .recipe-title", tmplRecipePage).html(data.title);
-    $("p.recipe-description", tmplRecipePage).html(data.description);
+    $("div.inner .recipe-title", tmplRecipeExcerpt).html(excerpt.title);
+    $("p.recipe-description", tmplRecipeExcerpt).html(excerpt.description);
 
     // _NOW_ we add this template to the index page
     $("#recipeExcerpts").append(tmplRecipeExcerpt);
